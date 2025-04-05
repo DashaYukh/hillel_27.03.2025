2e57cfe4ba041f39189d293ff398b555217caf67
Daryna <dash.yukh@gmail.com>
2025-04-05 21:58:32 +0300

    Commit

---
+++ Lesson 2/lesson.py
@@ -0,0 +1,16 @@
+x = int(input("enter a 4-digit number: "))
+print (x // 1000)
+print ((x // 100) % 10)
+print ((x // 10) % 10)
+print (x % 10)
+
+x = int(input("enter a 4-digit number: "))
+a = x // 1000
+b = (x // 100) % 10
+c = (x // 10) % 10
+d = (x % 10)
+
+print (a)
+print (b)
+print (c)
+print (d)
